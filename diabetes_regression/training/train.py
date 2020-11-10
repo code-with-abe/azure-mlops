c0b9f31a781fc5c61fbd93a95eac900a3e62a6f6
Abe <74078480+code-with-abe@users.noreply.github.com>
2020-11-09 21:50:19 -0600

    Added an update
    added a comment in line 38 to test automatic trigger

--- diabetes_regression/training/train.py
+++ diabetes_regression/training/train.py
@@ -35,7 +35,7 @@ from sklearn.model_selection import train_test_split
 def split_data(df):
     X = df.drop('Y', axis=1).values
     y = df['Y'].values
-
+# testing a change
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=0)
     data = {"train": {"X": X_train, "y": y_train},
